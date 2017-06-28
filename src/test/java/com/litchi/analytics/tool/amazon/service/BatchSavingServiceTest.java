70653f71e1e062a270a4d20419bd77974cfcad56
wenhaoquantil <wenhao.lin@quantil.com>
2017-06-28 08:24:00 -0700

    debug test case

--- src/test/java/com/litchi/analytics/tool/amazon/service/BatchSavingServiceTest.java
+++ src/test/java/com/litchi/analytics/tool/amazon/service/BatchSavingServiceTest.java
@@ -3,6 +3,8 @@ package com.litchi.analytics.tool.amazon.service;
 import com.litchi.analytics.tool.amazon.entity.AmzKeywordEntity;
 import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.repository.CrudRepository;
@@ -11,11 +13,13 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import static  org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BatchSavingServiceTest {
+    private  static Logger logger = LoggerFactory.getLogger(AmzCategoryService.class);
     @Autowired
     private AmzKeywordBatchSavingService batchSavingService;
     @Autowired
@@ -33,6 +37,12 @@ public class BatchSavingServiceTest {
         keywords.add(new AmzKeywordEntity().setSearch("search2").setWord("word2"));
         batchSavingService.batchSave(keywords);
         Object keyword = repo.findOne(1L);
+
+        if (keyword!=null) {
+            AmzKeywordEntity e = (AmzKeywordEntity) keyword;
+            logger.error(String.format("should be null, but not, %s: %s", e.getSearch(), e.getWord()));
+        }
+
         assertNull(keyword);
         batchSavingService.flush(keywords);
         Object saved = repo.findOne(2L);
